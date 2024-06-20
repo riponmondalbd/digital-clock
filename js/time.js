2ef3fcee85a1c13a2247df6c59fcbaf093a0d474
riponmondalbd <riponmondal149@gmail.com>
2024-06-21 03:33:41 +0600

    modefy template js

--- js/time.js
+++ js/time.js
@@ -16,7 +16,8 @@ function showTime() {
   m = m < 10 ? "0" + m : m;
   s = s < 10 ? "0" + s : s;
 
-  var time = h + ":" + m + ":" + s + " " + session;
+  //   var time = h + ":" + m + ":" + s + " " + session;
+  var time = `${h} : ${m} : ${s} ${session}`;
 
   document.getElementById("MyClockDisplay").innerText = time;
 
